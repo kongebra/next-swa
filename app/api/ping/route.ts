9d76ad36e76effea55d5535b3fd850b2a6a86536
Svein Are Danielsen <sveindani@gmail.com>
2024-03-08 13:39:50 +0100

    refactor: revalidate 0

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,9 +1,11 @@
 import { trace } from "@opentelemetry/api";
 import { NextResponse } from "next/server";
 
+export const revalidate = 0;
+
 export async function GET(req: Request) {
   return trace
-    .getTracer("test-tracer")
+    .getTracer("ping-tracer")
     .startActiveSpan("ping testing", async (span) => {
       span.setAttribute("foo", "bar");
 
